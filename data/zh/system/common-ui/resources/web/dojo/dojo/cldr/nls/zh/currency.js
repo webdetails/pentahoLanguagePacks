44b02bcf57b81d8cc8eed8483b7acf569c9abfe4
Carlos Russo <carlos.russo@webdetails.pt>
2014-01-29 16:33:29 +0000

    [zh] regenerated Chinese pack from 502ee

--- data/zh/system/common-ui/resources/web/dojo/dojo/cldr/nls/zh/currency.js
+++ data/zh/system/common-ui/resources/web/dojo/dojo/cldr/nls/zh/currency.js
@@ -1,17 +1,16 @@
 define(
     //begin v1.x content
     {
-        "HKD_displayName": "Hong Kong Dollar",
-        "CHF_displayName": "Swiss Franc",
-        "JPY_symbol": "¥",
-        "CAD_displayName": "Canadian Dollar",
-        "CNY_displayName": "Chinese Yuan",
-        "USD_symbol": "$",
-        "AUD_displayName": "Australian Dollar",
-        "JPY_displayName": "Japanese Yen",
-        "USD_displayName": "US Dollar",
-        "GBP_displayName": "British Pound Sterling",
-        "EUR_displayName": "Euro"
+        "HKD_displayName": "港元",
+        "CHF_displayName": "瑞士法郎",
+        "CAD_displayName": "加拿大元",
+        "CNY_displayName": "人民币",
+        "AUD_displayName": "澳大利亚元",
+        "JPY_displayName": "日元",
+        "USD_displayName": "美元",
+        "CNY_symbol": "￥",
+        "GBP_displayName": "英镑",
+        "EUR_displayName": "欧元"
     }
     //end v1.x content
 );
