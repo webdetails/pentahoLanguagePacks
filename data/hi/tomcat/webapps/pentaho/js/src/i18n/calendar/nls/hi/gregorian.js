9c051a6e51ea9e7ac9fb132ea33e51b6f0a62bbc
nkap00r <nikhil.kapoor@infoaxon.com>
2014-03-06 13:10:06 +0530

    Update to Hindi Translation files
    Covering the additional items

--- data/hi/tomcat/webapps/pentaho/js/src/i18n/calendar/nls/hi/gregorian.js
+++ data/hi/tomcat/webapps/pentaho/js/src/i18n/calendar/nls/hi/gregorian.js
@@ -1,4 +1,4 @@
-/*
+﻿/*
 	Copyright (c) 2004-2006, The Dojo Foundation
 	All Rights Reserved.
 
@@ -9,32 +9,32 @@
 */
 
 ({
-    "months-standAlone-narrow": ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"],
+    "months-standAlone-narrow": ["ज.", "फ.", "मा.", "अ.", "म.", "जू.", "जु.", "अ.", "सि.", "अ.", "न.", "दि."],
     "dateFormat-long": "MMMM d, yyyy",
     "timeFormat-full": "h:mm:ss a v",
-    "eras": ["BC", "AD"],
+    "eras": ["ई.पू.", "ई."],
     "timeFormat-medium": "h:mm:ss a",
     "dateFormat-medium": "MMM d, yyyy",
-    "months-format-abbr": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    "months-format-abbr": ["जन.", "फर.", "मार्च", "अप्रैल.", "मई", "जून", "जुल.", "अग.", "सित.", "अक्टू.", "नव.", "दिस."],
     "dateFormat-full": "EEEE, MMMM d, yyyy",
-    "days-format-abbr": ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    "days-format-abbr": ["रवि.", "सोम.", "मंगल.", "बुध.", "बृहस्पति.", "शुक्र.", "शनि."],
     "timeFormat-long": "h:mm:ss a z",
     "timeFormat-short": "h:mm a",
     "dateFormat-short": "M/d/yy",
-    "months-format-wide": ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-    "days-standAlone-narrow": ["S", "M", "T", "W", "T", "F", "S"],
-    "days-format-wide": ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-    "field-weekday": "Day of the Week",
-    "field-second": "Second",
-    "field-week": "Week",
-    "pm": "PM",
-    "am": "AM",
-    "field-year": "Year",
-    "field-minute": "Minute",
-    "field-hour": "Hour",
-    "field-day": "Day",
-    "field-dayperiod": "Dayperiod",
-    "field-month": "Month",
-    "field-era": "Era",
-    "field-zone": "Zone"
+    "months-format-wide": ["जनवरी", "फरवरी", "मार्च", "अप्रैल", "मई", "जून", "जुलाई", "अगस्त", "सितम्बर", "अक्टूबर", "नवम्बर", "दिसम्बर"],
+    "days-standAlone-narrow": ["र.", "सो.", "मं.", "बु.", "बृ.", "शु.", "श."],
+    "days-format-wide": ["रविवार", "सोमवार", "मंगलवार", "बुधवार", "बृहस्पतिवार", "शुक्रवार", "शनिवार"],
+    "field-weekday": "सप्ताह का दिन",
+    "field-second": "सेकंड",
+    "field-week": "सप्ताह",
+    "pm": "अपराह्न",
+    "am": "पूर्वाह्न",
+    "field-year": "वर्ष",
+    "field-minute": "मिनट",
+    "field-hour": "घंटा",
+    "field-day": "दिन",
+    "field-dayperiod": "दिनावधि",
+    "field-month": "महीना",
+    "field-era": "युग",
+    "field-zone": " ज़ोन"
 })
