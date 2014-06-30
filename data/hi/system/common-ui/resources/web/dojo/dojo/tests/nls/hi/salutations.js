5378c04c23698868116138c544e8a7a6435fad16
Carlos Russo <carlos.russo@webdetails.pt>
2014-06-30 10:17:28 +0100

    [hi] merged changes from Nikhil Kapoor from infoAxon

--- data/hi/system/common-ui/resources/web/dojo/dojo/tests/nls/hi/salutations.js
+++ data/hi/system/common-ui/resources/web/dojo/dojo/tests/nls/hi/salutations.js
@@ -1,4 +1,4 @@
-define(
+﻿define(
     //begin v1.x content
     {
         hi: "हिन्दी",
--- data/hi/system/reporting/reportviewer/dojo/dojo/nls/dojo-reportviewer_hi.js
+++ data/hi/system/reporting/reportviewer/dojo/dojo/nls/dojo-reportviewer_hi.js
@@ -1,51 +1,51 @@
-dojo.provide("dojo.nls.dojo-reportviewer_hi");
+﻿dojo.provide("dojo.nls.dojo-reportviewer_hi");
 dojo.provide("dijit.nls.loading");
 dijit.nls.loading._built = true;
 dojo.provide("dijit.nls.loading.hi");
 dijit.nls.loading.hi = {
-    "loadingState": "Loading...",
-    "errorState": "Sorry, an error occurred"
+   "loadingState": "लोड हो रहा है...",
+    "errorState": "खेद है, एक त्रुटि हुई"
 };
 dojo.provide("dijit.nls.common");
 dijit.nls.common._built = true;
 dojo.provide("dijit.nls.common.hi");
 dijit.nls.common.hi = {
-    "buttonOk": "OK",
-    "buttonCancel": "Cancel",
-    "buttonSave": "Save",
-    "itemClose": "Close"
+     "buttonOk": "ठीक है",
+    "buttonCancel": "रद्द करें",
+    "buttonSave": "सुरक्षित करें",
+    "itemClose": "बंद करें"
 };
 dojo.provide("dojo.cldr.nls.gregorian");
 dojo.cldr.nls.gregorian._built = true;
 dojo.provide("dojo.cldr.nls.gregorian.hi");
 dojo.cldr.nls.gregorian.hi = {
-    "dateFormatItem-yM": "M/y",
-    "field-dayperiod": "AM/PM",
+   "dateFormatItem-yM": "M/y",
+    "field-dayperiod": "पूर्वाह्न/अपराह्न",
     "dateFormatItem-yQ": "Q y",
-    "dayPeriods-format-wide-pm": "PM",
-    "field-minute": "Minute",
-    "eraNames": ["Before Christ", "Anno Domini"],
+    "dayPeriods-format-wide-pm": "पूर्वाह्न",
+    "field-minute": "मिनट",
+    "eraNames": ["ईसा पूर्व", "ईसवी"],
     "dateFormatItem-MMMEd": "E, MMM d",
     "dateTimeFormat-full": "{1} {0}",
-    "field-day-relative+-1": "Yesterday",
-    "field-weekday": "Day of the Week",
+    "field-day-relative+-1": "बीता हुआ कल ",
+    "field-weekday": "सप्ताह का दिन",
     "dateFormatItem-hms": "h:mm:ss a",
     "dateFormatItem-yQQQ": "QQQ y",
-    "days-standAlone-wide": ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+    "days-standAlone-wide": ["रविवार", "सोमवार", "मंगलवार", "बुधवार", "बृहस्पतिवार", "शुक्रवार", "शनिवार"],
     "dateFormatItem-MMM": "LLL",
-    "months-standAlone-narrow": ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"],
+    "months-standAlone-narrow": ["ज.", "फ.", "मा.", "अ.", "म.", "जू.", "जु.", "अ.", "सि.", "अ.", "न.", "दि."],
     "dateTimeFormat-short": "{1} {0}",
     "field-era": "Era",
     "field-hour": "Hour",
-    "dayPeriods-format-wide-am": "AM",
+    "dayPeriods-format-wide-am": "अपराह्न",
     "dateTimeFormat-medium": "{1} {0}",
     "dateFormatItem-y": "y",
     "timeFormat-full": "h:mm:ss a zzzz",
-    "months-standAlone-abbr": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    "months-standAlone-abbr": ["जन.", "फर.", "मार्च", "अप्रैल.", "मई", "जून", "जुला.", "अग.", "सित.", "अक्टू.", "नव.", "दिस."],
     "dateFormatItem-yMMM": "MMM y",
-    "field-day-relative+0": "Today",
-    "field-day-relative+1": "Tomorrow",
-    "days-standAlone-narrow": ["S", "M", "T", "W", "T", "F", "S"],
+    "field-day-relative+0": "आज",
+    "field-day-relative+1": "आने वाला कल",
+    "days-standAlone-narrow": ["र.", "सो.", "मं.", "बु.", "बृ.", "शु.", "श."],
     "eraAbbr": ["BC", "AD"],
     "dateFormat-long": "MMMM d, y",
     "timeFormat-medium": "h:mm:ss a",
@@ -54,66 +54,66 @@ dojo.cldr.nls.gregorian.hi = {
     "dateFormatItem-Hm": "HH:mm",
     "dateFormat-medium": "MMM d, y",
     "dateFormatItem-Hms": "HH:mm:ss",
-    "quarters-standAlone-wide": ["1st quarter", "2nd quarter", "3rd quarter", "4th quarter"],
+    "quarters-standAlone-wide": ["प्रथम तिमाही", "द्वितीय तिमाही", "तृतीय तिमाही", "चतुर्थ तिमाही"],
     "dateFormatItem-ms": "mm:ss",
     "field-year": "Year",
-    "quarters-standAlone-narrow": ["1", "2", "3", "4"],
+    "quarters-standAlone-narrow": ["प्र.", "द्वि.", "तृ.", "च."],
     "dateTimeFormat-long": "{1} {0}",
     "field-week": "Week",
-    "months-standAlone-wide": ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+    "months-standAlone-wide": ["जनवरी", "फरवरी", "मार्च", "अप्रैल", "मई", "जून", "जुलाई", "अगस्त", "सितम्बर", "अक्टूबर", "नवम्बर", "दिसम्बर"],
     "dateFormatItem-MMMd": "MMM d",
     "timeFormat-long": "h:mm:ss a z",
-    "months-format-abbr": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-    "dayPeriods-format-wide-noon": "noon",
+    "months-format-abbr": ["जन.", "फर.", "मार्च", "अप्रैल.", "मई", "जून", "जुला.", "अग.", "सित.", "अक्टू.", "नव.", "दिस."],
+    "dayPeriods-format-wide-noon": "मध्याह्न ",
     "timeFormat-short": "h:mm a",
     "field-month": "Month",
-    "quarters-format-abbr": ["Q1", "Q2", "Q3", "Q4"],
-    "days-format-abbr": ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    "quarters-format-abbr": ["ति.1", "ति.2", "ति.3", "ति.4"],
+    "days-format-abbr": ["रवि.", "सोम.", "मंगल.", "बुध.", "बृहस्पति.", "शुक्र.", "शनि."],
     "dateFormatItem-M": "L",
-    "days-format-narrow": ["S", "M", "T", "W", "T", "F", "S"],
-    "field-second": "Second",
-    "field-day": "Day",
+    "days-format-narrow": ["र.", "सो.", "मं.", "बु.", "बृ.", "शु.", "श."],
+    "field-second": "सेकंड",
+    "field-day": "दिन",
     "dateFormatItem-MEd": "E, M/d",
-    "months-format-narrow": ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"],
+    "months-format-narrow": ["ज.", "फ.", "मा.", "अ.", "म.", "जू.", "जु.", "अ.", "सि.", "अ.", "न.", "दि."],
     "dateFormatItem-hm": "h:mm a",
-    "days-standAlone-abbr": ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    "days-standAlone-abbr": ["रवि.", "सोम.", "मंगल.", "बुध.", "बृहस्पति.", "शुक्र.", "शनि."],
     "dateFormat-short": "M/d/yy",
     "dateFormatItem-yMMMEd": "EEE, MMM d, y",
     "dateFormat-full": "EEEE, MMMM d, y",
     "dateFormatItem-Md": "M/d",
     "dateFormatItem-yMEd": "EEE, M/d/y",
-    "months-format-wide": ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+    "months-format-wide": ["जनवरी", "फरवरी", "मार्च", "अप्रैल", "मई", "जून", "जुलाई", "अगस्त", "सितम्बर", "अक्टूबर", "नवम्बर", "दिसम्बर"],
     "dateFormatItem-d": "d",
-    "quarters-format-wide": ["1st quarter", "2nd quarter", "3rd quarter", "4th quarter"],
-    "days-format-wide": ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+    "quarters-format-wide": ["प्रथम तिमाही", "द्वितीय तिमाही", "तृतीय तिमाही", "चतुर्थ तिमाही"],
+    "days-format-wide": ["रविवार", "सोमवार", "मंगलवार", "बुधवार", "बृहस्पतिवार", "शुक्रवार", "शनिवार"],
     "eraNarrow": ["B", "A"],
     "dateTimeFormats-appendItem-Day-Of-Week": "{0} {1}",
-    "dayPeriods-format-abbr-am": "AM",
+    "dayPeriods-format-abbr-am": "अपराह्न",
     "dateTimeFormats-appendItem-Second": "{0} ({2}: {1})",
     "dateTimeFormats-appendItem-Era": "{0} {1}",
     "dateTimeFormats-appendItem-Week": "{0} ({2}: {1})",
     "dateFormatItem-H": "HH",
-    "quarters-standAlone-abbr": ["Q1", "Q2", "Q3", "Q4"],
+    "quarters-standAlone-abbr": ["ति.1", "ति.2", "ति.3", "ति.4"],
     "quarters-format-narrow": ["1", "2", "3", "4"],
     "dateFormatItem-h": "h a",
-    "dayPeriods-format-narrow-am": "AM",
+    "dayPeriods-format-narrow-am": "अपराह्न",
     "dateTimeFormats-appendItem-Day": "{0} ({2}: {1})",
     "dateTimeFormats-appendItem-Year": "{0} {1}",
     "dateTimeFormats-appendItem-Hour": "{0} ({2}: {1})",
-    "dayPeriods-format-abbr-pm": "PM",
+    "dayPeriods-format-abbr-pm": "पूर्वाह्न",
     "dateTimeFormats-appendItem-Quarter": "{0} ({2}: {1})",
     "dateTimeFormats-appendItem-Month": "{0} ({2}: {1})",
     "dateTimeFormats-appendItem-Minute": "{0} ({2}: {1})",
     "dateTimeFormats-appendItem-Timezone": "{0} {1}",
-    "dayPeriods-format-narrow-pm": "PM"
+    "dayPeriods-format-narrow-pm": "पूर्वाह्न"
 };
 dojo.provide("dijit.form.nls.validate");
 dijit.form.nls.validate._built = true;
 dojo.provide("dijit.form.nls.validate.hi");
 dijit.form.nls.validate.hi = {
-    "rangeMessage": "This value is out of range.",
-    "invalidMessage": "The value entered is not valid.",
-    "missingMessage": "This value is required."
+   "rangeMessage": "यह मान सीमा के बाहर है.",
+    "invalidMessage": "प्रविष्ट मान वैध नहीं है.",
+    "missingMessage": "यह मान आवश्यक है."
 };
 dojo.provide("dojo.cldr.nls.number");
 dojo.cldr.nls.number._built = true;
