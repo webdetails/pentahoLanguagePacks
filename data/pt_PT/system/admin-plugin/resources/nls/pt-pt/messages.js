a09e54e637acd11d1e3bff674e4560c0a98440ec
marciojv <marcio@ambientelivre.com.br>
2015-07-23 22:30:23 -0300

    translate analizer, jpivot(partial)

--- data/pt_PT/system/admin-plugin/resources/nls/pt-pt/messages.js
+++ data/pt_PT/system/admin-plugin/resources/nls/pt-pt/messages.js
@@ -47,7 +47,7 @@ define({
     //ldap custom user search configuration
     LDAP_USER_SEARCH_TITLE: 'Pesquisa de Utilizador',
 
-    //ldap custom roles configuration 
+    //ldap custom roles configuration
     LDAP_ROLES_TITLE: 'Funções',
     LDAP_ROLE_BASE_LABEL: 'Atributo da Função',
     LDAP_ROLE_SEARCH_FILTER_LABEL: 'Filtro de Pesquisa de Função:',
@@ -119,8 +119,7 @@ define({
     LICENSE_NO_VALID_LICENSE_FOUND_LOG: 'Nenhuma licença Biserver-EE válida encontrada',
     LICENSE_NO_VALID_LICENSE_MESSAGE_HEADER: "Licença ausente ou expirada.",
     LICENSE_NO_VALID_LICENSE_MESSAGE_BODY: 'Para continuar é necessário atualizar a sua licença Pentaho BI Platform. Se ainda não estiver preparado pode <a href="%CONTEXT_PATH%\Logout" target="_top">terminar a sessão</a> e voltar mais tarde.',
-    < TRANSLATE ME >
-        LICENSE_CANCEL_BUTTON: 'Terminar a Sessão',
+    LICENSE_CANCEL_BUTTON: 'Terminar a Sessão',
     LICENSE_OK_BUTTON: 'Ok'
 
 });
