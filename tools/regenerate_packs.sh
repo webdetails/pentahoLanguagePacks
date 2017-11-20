4a88ff6ea97966c129f4af159e10f8abee40c934
Carlos Russo <crusso@pentaho.com>
2017-11-20 11:05:38 +0000

    [all] Cleaned up build

--- tools/regenerate_packs.sh
+++ tools/regenerate_packs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./generate-language.sh ar ca cs da de en en_US es es_ES es_MX el fi fr gl_GL hi hr hu it ja ko lt nl no pl pt_BR pt_PT ro ru sl sk sq_AL sv ta th tlh tr uk zh zh_CN zh_TW
+./generate-language.sh ar ca cs da de en en_US es es_ES es_MX el fi fr gl hi hr hu it ja ko lt nl no pl pt_BR pt_PT ro ru sl sk sq_AL sv ta th tlh tr uk zh zh_CN zh_TW
